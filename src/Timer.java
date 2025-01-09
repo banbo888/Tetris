01a0db0cb18d9c01103429085eae5dc8da44ce16
banbo888 <apollomusic2000@gmail.com>
2025-01-09 00:05:36 -0500

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -37,36 +37,43 @@ public class Timer {
 
     // Get the time remaining from 2 minutes (used for Time-Trial mode)
     public String getTimeRemaining() {
-        long elapsedTime = getElapsedTime();
-        long remainingTime = 120000 - elapsedTime;
+        long elapsedTime, remainingTime, minutes, seconds, milliseconds;
+
+        elapsedTime = getElapsedTime();
+        remainingTime = 120000 - elapsedTime;
 
         // Clamp to 0 if time has run out
         remainingTime = Math.max(remainingTime, 0);
 
-        long minutes = (remainingTime / 60000) % 60;
-        long seconds = (remainingTime / 1000) % 60;
-        long milliseconds = remainingTime % 1000;
+        minutes = (remainingTime / 60000) % 60;
+        seconds = (remainingTime / 1000) % 60;
+        milliseconds = remainingTime % 1000;
 
         return String.format("%02d:%02d.%03d", minutes, seconds, milliseconds);
     }
 
     // Format elapsed time as "MM:SS:mmm"
     public String getFormattedTime() {
-        long elapsedTime = getElapsedTime();
+        long elapsedTime, minutes, seconds, milliseconds;
 
-        long minutes = (elapsedTime / 60000) % 60;   // 1 minute = 60,000 ms
-        long seconds = (elapsedTime / 1000) % 60;    // 1 second = 1,000 ms
-        long milliseconds = elapsedTime % 1000;
+        elapsedTime = getElapsedTime();
+
+        minutes = (elapsedTime / 60000) % 60;   // 1 minute = 60,000 ms
+        seconds = (elapsedTime / 1000) % 60;    // 1 second = 1,000 ms
+        milliseconds = elapsedTime % 1000;
 
         return String.format("%02d:%02d.%03d", minutes, seconds, milliseconds);
     }
 
     // Change string time to long time
     public static long parseTimeToMillis(String time) {
-        String[] parts = time.split(":");
-        int minutes = Integer.parseInt(parts[0]);
-        int seconds = Integer.parseInt(parts[1]);
-        int millis = Integer.parseInt(parts[2]);
+        String[]parts;
+        int minutes, seconds, millis;
+
+        parts = time.split(":");
+        minutes = Integer.parseInt(parts[0]);
+        seconds = Integer.parseInt(parts[1]);
+        millis = Integer.parseInt(parts[2]);
         return (minutes * 60 * 1000L) + (seconds * 1000L) + millis;
     }
 }

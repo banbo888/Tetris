c87c9ea2412b4fc363cda75e740fc1b6068efaca
ericmxf17 <138934475+ericmxf17@users.noreply.github.com>
2025-01-09 03:14:51 -0500

    Create runMe.bat

---
+++ runMe.bat
@@ -0,0 +1,18 @@
+javac Grid.java
+javac GamePanel.java
+javac GameInterface.java
+javac GameFrame.java
+javac ExitScreen.java
+javac Tetromino.java
+javac SoundManager.java
+javac SettingsManager.java
+javac ScoreManager.java
+javac PieceBagGenerator.java
+javac PauseScreen.ava
+javac MenuScreen.java
+javac Main.java
+javac Timer.java
+javac ScoreScreen.java
+pause
+java Main
+pause

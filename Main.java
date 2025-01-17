9ef2eab2537329775bf3afe719893df4a0fe771e
banbo888 <apollomusic2000@gmail.com>
2025-01-17 08:57:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 //ICS Summative - Tetris by Richard Xiong & Eric Ma
-//Beta Program Submission
-//2025-01-09
+//Final Program Submission
+//2025-01-16
 //Main Class - Starts/Initializes the Tetris game by creating a new GameFrame.
 
 public class Main {
